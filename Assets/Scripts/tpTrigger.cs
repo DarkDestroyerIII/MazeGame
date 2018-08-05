8397df6f02307e9e954a55b7f25b3b8d13fe8514
Ichiro <Ichiro>
2018-08-05 15:58:34 -0700

    Initial Commit

---
+++ Assets/Scripts/tpTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tpTrigger : MonoBehaviour {
+
+	public string tagToCheck;
+
+	public Vector3 positionOffset;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider otherCollider){
+
+		if (otherCollider.gameObject.CompareTag (tagToCheck)) {
+
+			otherCollider.transform.position = otherCollider.transform.position + positionOffset;
+
+
+		}
+
+
+	}
+}
---
+++ Assets/Scripts/triggerFlag.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerFlag : MonoBehaviour {
+
+	public GameObject gameObjectToEnable;
+	public string tagToCheck;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider otherCollider){
+
+		if (otherCollider.gameObject.CompareTag (tagToCheck)) {
+
+			gameObjectToEnable.SetActive (true);
+		}
+
+		
+	}
+
+}
